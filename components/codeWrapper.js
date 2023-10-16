f347ba086e184a2fd2bfe4473ed6bad9ffd400e8
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-10-16 09:25:45 +0300

    Add codeWrapper component to project

---
+++ components/codeWrapper.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const codeWrapper = () => {
+ return (
+  <div class="code-wrapper">
+   <div class="code-header">
+    <span class="dot red"></span>
+    <span class="dot yellow"></span>
+    <span class="dot green"></span>
+   </div>
+   <div class="code-content">
+    <!-- Your content here -->
+   </div>
+  </div>
+ )
+}
+
+export default codeWrapper
